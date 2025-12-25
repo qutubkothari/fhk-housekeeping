7b527384a087d5d1ee0e0ccda644a91050e2b07f
FHK Developer <dev@fhk.com>
2025-12-25 16:15:10 +0530

    deploy: Quick deploy commit - 2025-12-25 16:15:09

--- apps/staff-pwa/vite.config.js
+++ apps/staff-pwa/vite.config.js
@@ -35,6 +35,9 @@ export default defineConfig({
         ],
       },
       workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        cleanupOutdatedCaches: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
